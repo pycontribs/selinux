4e6cc7274003d955d539ef8a19130692f4862adb
Charlton Stanley <charlton@charltonstanley.com>
2022-11-29 09:59:40 -0600

    fix: updated formatting to be aligned with `black`

--- selinux/__init__.py
+++ selinux/__init__.py
@@ -73,7 +73,9 @@ if should_have_selinux():
         """Get sitepackage locations from system python"""
         # Do not ever use sys.executable here
         # See https://github.com/pycontribs/selinux/issues/17 for details
-        system_python = "/usr/bin/python%s" % ".".join([str(item) for item in platform.python_version_tuple()[0:2]])
+        system_python = "/usr/bin/python%s" % ".".join(
+            [str(item) for item in platform.python_version_tuple()[0:2]]
+        )
 
         system_sitepackages = json.loads(
             subprocess.check_output(
